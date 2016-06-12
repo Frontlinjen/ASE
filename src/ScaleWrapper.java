54fb5fbe00b6abaa58131dfc86617f0bd19205ac
Thomas Bech Madsen <thomasbm@live.dk>
2016-06-12 11:30:36 +0200

    Added ScaleWrapper

---
+++ src/ScaleWrapper.java
@@ -0,0 +1,20 @@
+
+public class ScaleWrapper {
+
+	public String getInput() {
+		return "D";
+	}
+
+	public void pushDisplay(String string) {
+		
+	}
+
+	public void tara() {
+		
+	}
+
+	public double getWeight() {
+		return 0;
+	}
+
+}
