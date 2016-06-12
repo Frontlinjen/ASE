6682f380b152ea713e2ee50732ad2205e4601fd4
Thomas Bech Madsen <thomasbm@live.dk>
2016-06-12 19:56:24 +0200

    Changed name of Main to Runner Added new class Main

--- src/Main.java
+++ src/Main.java
@@ -1,107 +1,19 @@
+import java.io.IOException;
+import java.net.Socket;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) {
+		try {
+			ConnectionHandler connector = new ConnectionHandler(0); //portnummer?
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		
+		
+		new Thread(new Runner()).start();
 	}
 
-	
-	public void operatorIdentifier(){ //punkt 1-4
-		MySQLAnsatDAO database = new MySQLAnsatDAO();
-		String input = null;
-		
-		while(true){
-			//Ask ScaleWrapper for id
-			while(input == null){
-				input = ScaleWrapper.getInput();
-			}
-			try {
-				ScaleWrapper.pushDisplay("Hello " + database.getAnsat(input).getOprNavn() + ", please confirm (1: yes/2: no)");
-				while(input == null){
-					input = ScaleWrapper.getInput();
-				}
-				if(input == "1"){
-					return;
-				}
-				else if(input == "2"){
-					operatorIdentifier();
-				}
-				
-			} catch (DALException e) {
-				if(input.length() < 10){
-					ScaleWrapper.pushDisplay("Id too short");
-				}
-				else if(input.length() > 10){
-					ScaleWrapper.pushDisplay("Id too long");
-				}
-				else{
-					ScaleWrapper.pushDisplay("User not found");
-				}
-			}
-		}
-	}
-	
-	public void afvejning(){ //punkt 5-16
-		MySQLProduktbatchDAO pbDAO = new MySQLProduktbatchDAO();
-		MySQLReceptDAO rcDAO = new MySQLReceptDAO();
-		String input = null;
-		int produktBatchNr;
-		ScaleWrapper.pushDisplay("Please state the product batch number:");
-		while(input == null){
-			input = ScaleWrapper.getInput();
-		}
-		produktBatchNr = Integer.parseInt(input);
-		try{
-		ScaleWrapper.pushDisplay(rcDAO.getRecept(pbDAO.getProduktBatch(produktBatchNr).getReceptId()).getReceptNavn() + ", please confirm the recipe (1: yes/2: no)");
-		while(input == null){
-			input = ScaleWrapper.getInput();
-		}
-		if(input == "1"){
-			afvejningCore(produktBatchNr);
-		}
-		else if(input == "2"){
-			afvejning();
-		}
-		}
-		catch(DALException e){
-			ScaleWrapper.pushDisplay("Failed to find recipe, please check product batch number");
-			afvejning();
-			//Annuller mulighed?
-		}
-	}
-	
-	private void afvejningCore(int produktBatch){
-		MySQLProduktBatchKompDAO pbkDAO = new MySQLProduktBatchKompDAO();
-		MySQLProduktbatchDAO pbDAO = new MySQLProduktbatchDAO();
-		String input = null;
-		try{
-			for(int i = 0; i < pbkDAO.getSpecifiedProduktBatchKompList(Integer.parseInt(input)).size()/*Lav en SpecifiedProduktBatchKomponentList(String); i DAO*/; i++){
-				ScaleWrapper.pushDisplay("Please confirm that the ScaleWrapper is clear, press 1 to confirm");
-				while(input != "1"){
-					ScaleWrapper.getInput();
-				}
-				pbDAO.getProduktBatch(produktBatch).setStatus(1);
-				ScaleWrapper.pushDisplay("AutoTara"); //Yes?
-				ScaleWrapper.tara();
-				ScaleWrapper.pushDisplay("Please place container on the ScaleWrapper and press 1 when ready");
-				while(input != "1"){
-					ScaleWrapper.getInput();
-				}
-				double containerWeight = ScaleWrapper.getWeight();
-				ScaleWrapper.pushDisplay("AutoTara"); //Yes?
-				ScaleWrapper.tara();
-				ScaleWrapper.pushDisplay("Please weigh component with resourcebatch id = " + pbkDAO.getProduktBatchKompList().get(i).getRaavarebatchId() + " in the container and press 1 when ready");
-				while(input != "1"){
-					ScaleWrapper.getInput();
-				}
-				pbkDAO.getProduktBatchKompList().get(i).setTara(containerWeight);
-				pbkDAO.getProduktBatchKompList().get(i).setNetto(ScaleWrapper.getWeight()-containerWeight);
-			}
-			pbDAO.getProduktBatch(produktBatch).setStatus(2);
-		}
-		catch(DALException e){
-			ScaleWrapper.pushDisplay("Error happened, trying again...");
-			afvejningCore(produktBatch);
-		}
-	}
 }
---
+++ src/Runner.java
@@ -0,0 +1,113 @@
+
+public class Runner implements Runnable{
+
+	@Override
+	public void run() {
+		operatorIdentifier();
+		afvejning();
+	}
+	
+	private void operatorIdentifier(){ //punkt 1-4
+		ScaleWrapper scale = new ScaleWrapper();
+		MySQLAnsatDAO database = new MySQLAnsatDAO();
+		String input = null;
+		
+		while(true){
+			//Ask ScaleWrapper for id
+			while(input == null){
+				input = scale.getInput();
+			}
+			try {
+				scale.pushDisplay("Hello " + database.getAnsat(input).getOprNavn() + ", please confirm (1: yes/2: no)");
+				while(input == null){
+					input = scale.getInput();
+				}
+				if(input == "1"){
+					return;
+				}
+				else if(input == "2"){
+					operatorIdentifier();
+				}
+				
+			} catch (DALException e) {
+				if(input.length() < 10){
+					scale.pushDisplay("Id too short");
+				}
+				else if(input.length() > 10){
+					scale.pushDisplay("Id too long");
+				}
+				else{
+					scale.pushDisplay("User not found");
+				}
+			}
+		}
+	}
+	
+	private void afvejning(){ //punkt 5-16
+		ScaleWrapper scale = new ScaleWrapper();
+		MySQLProduktbatchDAO pbDAO = new MySQLProduktbatchDAO();
+		MySQLReceptDAO rcDAO = new MySQLReceptDAO();
+		String input = null;
+		int produktBatchNr;
+		
+		scale.pushDisplay("Please state the product batch number:");
+		while(input == null){
+			input = scale.getInput();
+		}
+		produktBatchNr = Integer.parseInt(input);
+		try{
+		scale.pushDisplay(rcDAO.getRecept(pbDAO.getProduktBatch(produktBatchNr).getReceptId()).getReceptNavn() + ", please confirm the recipe (1: yes/2: no)");
+		while(input == null){
+			input = scale.getInput();
+		}
+		if(input == "1"){
+			afvejningCore(produktBatchNr);
+		}
+		else if(input == "2"){
+			afvejning();
+		}
+		}
+		catch(DALException e){
+			scale.pushDisplay("Failed to find recipe, please check product batch number");
+			afvejning();
+			//Annuller mulighed?
+		}
+	}
+	
+	private void afvejningCore(int produktBatch){
+		ScaleWrapper scale = new ScaleWrapper();
+		MySQLProduktBatchKompDAO pbkDAO = new MySQLProduktBatchKompDAO();
+		MySQLProduktbatchDAO pbDAO = new MySQLProduktbatchDAO();
+		String input = null;
+		try{
+			for(int i = 0; i < pbkDAO.getSpecifiedProduktBatchKompList(Integer.parseInt(input)).size()/*Lav en SpecifiedProduktBatchKomponentList(String); i DAO*/; i++){
+				scale.pushDisplay("Please confirm that the ScaleWrapper is clear, press 1 to confirm");
+				while(input != "1"){
+					scale.getInput();
+				}
+				pbDAO.getProduktBatch(produktBatch).setStatus(1);
+				scale.pushDisplay("AutoTara"); //Yes?
+				scale.tara();
+				scale.pushDisplay("Please place container on the ScaleWrapper and press 1 when ready");
+				while(input != "1"){
+					scale.getInput();
+				}
+				double containerWeight = scale.getWeight();
+				scale.pushDisplay("AutoTara"); //Yes?
+				scale.tara();
+				scale.pushDisplay("Please weigh component with resourcebatch id = " + pbkDAO.getProduktBatchKompList().get(i).getRaavarebatchId() + " in the container and press 1 when ready");
+				while(input != "1"){
+					scale.getInput();
+				}
+				pbkDAO.getProduktBatchKompList().get(i).setTara(containerWeight);
+				pbkDAO.getProduktBatchKompList().get(i).setNetto(scale.getWeight()-containerWeight);
+			}
+			pbDAO.getProduktBatch(produktBatch).setStatus(2);
+		}
+		catch(DALException e){
+			scale.pushDisplay("Error happened, trying again...");
+			afvejningCore(produktBatch);
+		}
+	}
+
+}
