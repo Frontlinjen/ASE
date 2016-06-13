ff38546bf75c843b38b49203e3668d92fc67e1cc
Hjorthen <Sebastian@10.16.101.25>
2016-06-13 11:53:42 +0200

    Made main connect to a list of IP addresses

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
-import java.io.IOException;
-import java.net.Socket;
-
 public class Main{
 
 	public static void main(String[] args) {
-		try {
-			ConnectionHandler connector = new ConnectionHandler(0); //portnummer?
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		final String IPAdresses[] = {
+				"168.1.1.23",
+				"localhost",	
+		};
+		for (String string : IPAdresses) {
+			new Thread(new Runner(string)).start();	
 		}
-		
-		
-		
-		new Thread(new Runner()).start();
 	}
 
 }
