392d2aab05948a6bba208564126b4c54ef961755
Thomas Bech Madsen <thomasbm@live.dk>
2016-06-16 22:52:28 +0200

    Removed SQLStates

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main{
 
 	public static void main(String[] args) {
 		final String IPAdresses[] = {
-				"169.254.2.3",
+//				"169.254.2.3",
 //				"169.254.2.2",
 //				"169.254.2.1",
-//				"localhost",	
+				"localhost",	
 		};
 		for (String string : IPAdresses) {
 			new Thread(new Runner(string)).start();	
